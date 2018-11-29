00521b4865b7e26174ea4d068d1b730192ee4b8f
nik <nik@nik-PC>
2018-11-29 22:25:58 +0100

    Merge origin/master
    Conflicts:
    	src/view/FrameGenerik.java

--- src/params/ColorChart.java
+++ src/params/ColorChart.java
@@ -12,36 +12,38 @@ import java.awt.Font;
  *
  * @author nrochas
  */
-public class ColorChart {
-    
-    
+public class ColorChart
+{
+
     public static Color colorBack;
     public static Color colorFont;
     public static Color colorFront;
-    public static Font titre1;
+    public static Font fontCorps;
+    public static Font fontButton;
     public static Font titre3;
     public static Font titre2;
-     public static Font fontCorps;
-     public static Font fontButton;
+    public static Font titre1;
 
-    private ColorChart() {
-        colorBack = new Color(0,156,180);
-        colorFront = new Color(251,187,0);
-        colorFont = new Color(76,79,83);
-        titre3 = new Font("TAHOMA",Font.BOLD, 13);
-        fontCorps = new Font("TAHOMA",Font.PLAIN,12);
+    private ColorChart()
+    {
+        colorBack = new Color(0, 156, 180);
+        colorFront = new Color(251, 187, 0);
+        colorFont = new Color(76, 79, 83);
+        fontCorps = new Font("Tekton pro Ext", Font.PLAIN, 12);
+        titre3 = fontCorps.deriveFont(Font.BOLD, 13);
+
         fontButton = fontCorps.deriveFont(13);
+        titre2 = fontCorps.deriveFont(Font.BOLD,15);
+        titre1 = fontCorps.deriveFont(Font.BOLD,18);
     }
 
-   
-    
-    
-    
-    public static ColorChart getInstance() {
+    public static ColorChart getInstance()
+    {
         return ColorChartHolder.INSTANCE;
     }
-    
-    private static class ColorChartHolder {
+
+    private static class ColorChartHolder
+    {
 
         private static final ColorChart INSTANCE = new ColorChart();
     }
